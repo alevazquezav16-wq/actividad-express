a9bd358d84cf50fdaeb11a1aab06de45919a7a2c
Alejandro Vladimir Vazquez Valles <alevazquez.av16@gmail.com>
2026-03-24 23:21:56 -0700

    Agregando endpoint /usuario

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 
+// Ruta principal
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API funcionando');
 });
 
+// NUEVA RUTA
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Alejandro',
+    rol: 'Estudiante'
+  };
+
+  res.json(usuario);
+});
+
+// Servidor
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
-})
+});
+
