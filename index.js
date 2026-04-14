53486b89310a7676dd60367948f4cfa40e0fc0d9
Alejandro Vladimir Vazquez Valles <alevazquez.av16@gmail.com>
2026-04-14 06:55:10 -0700

    Primer SELECT de alumnos con PostgreSQL y Express

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const pool = require('./db'); // 👈 SIEMPRE ARRIBA
+
 const app = express();
+
+app.use(express.json());
 
 // Ruta principal
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
-// NUEVA RUTA
+// Ruta usuario (ya la tienes)
 app.get('/usuario', (req, res) => {
   const usuario = {
     id: 1,
@@ -17,13 +21,18 @@ app.get('/usuario', (req, res) => {
   res.json(usuario);
 });
 
-// Servidor
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+// (NUEVA RUTA)
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
 });
 
-const pool = require('./db');
-
+// Conexión a PostgreSQL
 pool.connect()
   .then(() => {
     console.log('Conexión exitosa a PostgreSQL');
@@ -31,3 +40,8 @@ pool.connect()
   .catch((err) => {
     console.error('Error de conexión', err);
   });
+
+// Servidor
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
